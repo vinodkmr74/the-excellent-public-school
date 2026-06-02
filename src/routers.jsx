2152fb0b8dd5f610efd91c4cceebed3c3fc8bef1
Vinod Kumar <vinod stm74@gmail.com>
2026-06-02 19:35:07 +0530

    new

--- src/routers.jsx
+++ src/routers.jsx
@@ -5,12 +5,12 @@ import {
   Route,
 } from "react-router-dom";
 import App from "./App";
-import Home from "./page/home/Home";
 import About from "./page/about/about";
 import Academic from "./page/academic/Academic";
 import Admition from "./page/admition/Admition";
 import Contact from "./page/contact/Contact";
 import Blogs from "./page/blogs/Blogs";
+import Home from "./page/home/Home";
 
 
 export default function Routers() {
@@ -18,7 +18,7 @@ export default function Routers() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route path="/" element={<Home /> }/>
+          <Route path="/" element={<Home />}/>
           <Route path="/about" element={<About />}/>
           <Route path="/academic" element={<Academic />} />
           <Route path="/admition" element={<Admition />} />
